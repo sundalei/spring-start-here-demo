5057bfcf0f0b8b0a10c3ab9d17902893baab1bf6
Leo Sun <leosun@Leos-MacBook-Pro.local>
2022-11-13 21:35:00 +0800

    multiple beans with same type

---
+++ chapter2-demo-multi-bean-with-same-type/src/main/java/com/example/App.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.config.ProjectConfig;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+		
+		Parrot p = context.getBean(Parrot.class);
+		
+		System.out.println(p.getName());
+	}
+}
--- chapter2-demo/src/main/java/com/example/Main.java
+++ chapter2-demo/src/main/java/com/example/Main.java
@@ -2,12 +2,20 @@ package com.example;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.example.config.ProjectConfig;
+
 public class Main {
     public static void main(String[] args) {
 
-        var context = new AnnotationConfigApplicationContext();
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
         
-        Parrot parrot = new Parrot();
-        System.out.println("Hello World!");
+        Parrot parrot = context.getBean(Parrot.class);
+        System.out.println(parrot.getName());
+        
+        String s = context.getBean(String.class);
+        System.out.println(s);
+        
+        Integer n = context.getBean(Integer.class);
+        System.out.println(n);
     }
 }
