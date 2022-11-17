b7acc1affe01237166056ca91f209aa02f3584f8
Leo Sun <leosun@Leos-MacBook-Pro.local>
2022-11-17 21:20:14 +0800

    chapter 3

---
+++ chapter3-bean-dependency-with-parameter/src/main/java/com/example/config/ProjectConfig.java
@@ -0,0 +1,26 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.Parrot;
+import com.example.Person;
+
+@Configuration
+public class ProjectConfig {
+    
+    @Bean
+    public Parrot parrot() {
+        Parrot p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+
+    @Bean
+    public Person person(Parrot parrot) {
+        Person p = new Person();
+        p.setName("Ella");
+        p.setParrot(parrot);
+        return p;
+    }
+}
