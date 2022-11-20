7f5ef294cd7b15c96f53c6ab00d97f752c1e9651
DaLei Sun <sundalei1988@gmail.com>
2022-11-20 16:39:40 +0800

    chapter3

---
+++ chapter3-circular-demo/src/main/java/com/example/Person.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+    private String name = "Ella";
+    private final Parrot parrot;
+
+    @Autowired
+    public Person(Parrot parrot) {
+        this.parrot = parrot;
+    }
+
+    public Parrot getParrot() {
+        return parrot;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
