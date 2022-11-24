7bc13e9b7f48eda0fbf82c83fcdc89fd5827f0e7
Leo Sun <sundalei1988@gmail.com>
2022-11-24 10:06:58 +0800

    chapter4

--- chapter4-demo/src/main/java/com/example/main/Main.java
+++ chapter4-demo/src/main/java/com/example/main/Main.java
@@ -1,23 +1,24 @@
 package com.example.main;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.config.ProjectConfiguration;
 import com.example.model.Comment;
-import com.example.proxies.EmailCommentNotificationProxy;
-import com.example.repositories.DBCommentRepository;
 import com.example.service.CommentService;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        var commentRepository = new DBCommentRepository();
-        var commentNotificationProxy = new EmailCommentNotificationProxy();
-
-        var commentService = new CommentService(commentRepository, commentNotificationProxy);
+        var context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
 
         var comment = new Comment();
         comment.setAuthor("Laurentiu");
         comment.setText("Demo comment");
 
+        var commentService = context.getBean(CommentService.class);
         commentService.publishComment(comment);
+
+        context.close();
     }
 }
