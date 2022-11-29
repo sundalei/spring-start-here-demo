215055ce0b16eaf41821c0ffa0e786ced7b4ddcc
DaLei Sun <sundalei1988@gmail.com>
2022-11-29 17:45:14 +0800

    aspects

---
+++ chapter6-aspect/src/main/java/com/example/services/CommentService.java
@@ -0,0 +1,17 @@
+package com.example.services;
+
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import com.example.model.Comment;
+
+@Service
+public class CommentService {
+    
+    private Logger logger = Logger.getLogger(CommentService.class.getName());
+
+    public void publishComment(Comment comment) {
+        logger.info("Publishing comment: " + comment.getText());
+    }
+}
