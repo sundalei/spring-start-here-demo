a08fa436851ccbaa24aa6dbc18a9becb11b9fad3
Leo Sun <sundalei1988@gmail.com>
2022-11-30 10:45:17 +0800

    aspect

--- chapter6-aspect/src/main/java/com/example/App.java
+++ chapter6-aspect/src/main/java/com/example/App.java
@@ -1,13 +1,24 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.example.config.ProjectConfig;
+import com.example.model.Comment;
+import com.example.services.CommentService;
+
+public class App {
+    public static void main(String[] args) {
+
+        var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        var service = c.getBean(CommentService.class);
+
+        Comment comment = new Comment();
+        comment.setText("Demo comment");
+        comment.setAuthor("Natasha");
+
+        service.publishComment(comment);
+
+        c.close();
     }
 }
---
+++ chapter6-aspect/src/main/java/com/example/config/LoggingAspect.java
@@ -0,0 +1,20 @@
+package com.example.config;
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class LoggingAspect {
+
+    private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
+    
+    @Around("execution(* com.example.services.*.*(..))")
+    public void log(ProceedingJoinPoint joinPoint) throws Throwable {
+        logger.info("Method will execute");
+        joinPoint.proceed();
+        logger.info("Method executed");
+    }
+}
