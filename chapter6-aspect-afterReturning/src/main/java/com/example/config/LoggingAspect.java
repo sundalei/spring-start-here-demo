54053d000f76bda4efc4b51ed58b866133d54a0a
Leo Sun <leosun@Leos-MacBook-Pro.local>
2022-12-04 16:59:14 +0800

    aspect after returning

---
+++ chapter6-aspect-afterReturning/src/main/java/com/example/config/LoggingAspect.java
@@ -0,0 +1,20 @@
+package com.example.config;
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class LoggingAspect {
+    
+    private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
+
+    // @Around("execution(* com.example.services.*.*(..))")
+    @AfterReturning(value = "@annotation(com.example.ToLog)", returning = "returnedValue")
+    public Object log(Object returnedValue) {
+
+        logger.info("Method executed and returned " + returnedValue);
+        return returnedValue;
+    }
+}
