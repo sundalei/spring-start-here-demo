fa5a0bc2569c01bbb09942d02ed595f5e9bbb7e8
Leo Sun <leosun@Leos-MacBook-Pro.local>
2022-12-06 22:33:38 +0800

    chapter7

---
+++ chapter7-first-web/src/main/java/com/example/Chapter7FirstWebApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter7FirstWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter7FirstWebApplication.class, args);
+	}
+
+}
---
+++ chapter7-first-web/src/test/java/com/example/Chapter7FirstWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter7FirstWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
