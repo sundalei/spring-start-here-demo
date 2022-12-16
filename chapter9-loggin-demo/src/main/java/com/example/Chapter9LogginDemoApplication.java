f38fb0f67e1cf8b991fcf446751412fa3acd15ca
sundalei <sundalei1988@gmail.com>
2022-12-16 21:42:30 +0800

    web scopes

---
+++ chapter9-loggin-demo/src/main/java/com/example/Chapter9LogginDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter9LogginDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter9LogginDemoApplication.class, args);
+	}
+
+}
---
+++ chapter9-loggin-demo/src/test/java/com/example/Chapter9LogginDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter9LogginDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
