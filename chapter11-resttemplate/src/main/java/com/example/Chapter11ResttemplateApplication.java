0e439fdecbd02f7ef6d827f0a3b78ed57a421196
Leo Sun <leosun@Leos-MacBook-Pro.local>
2022-12-26 10:33:27 +0800

    rest template

---
+++ chapter11-resttemplate/src/main/java/com/example/Chapter11ResttemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter11ResttemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter11ResttemplateApplication.class, args);
+	}
+
+}
---
+++ chapter11-resttemplate/src/main/java/com/example/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+import com.example.model.Payment;
+import com.example.proxy.PaymentProxy;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+
+    private final PaymentProxy paymentProxy;
+
+    public PaymentController(PaymentProxy paymentProxy) {
+        this.paymentProxy = paymentProxy;
+    }
+
+    @PostMapping("/payment")
+    public Payment createPayment(
+            @RequestBody Payment payment
+    ) {
+        return paymentProxy.createPayment(payment);
+    }
+}
