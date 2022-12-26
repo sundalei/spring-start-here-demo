4f8aab01d78b752b06613dc18f5e42f9dc8f8da6
Leo Sun <leosun@Leos-MacBook-Pro.local>
2022-12-26 14:56:51 +0800

    webclient

--- chapter11-webclient/src/main/java/com/example/config/ProjectConfig.java
+++ chapter11-webclient/src/main/java/com/example/config/ProjectConfig.java
@@ -1,2 +1,12 @@
-package com.example.config;public class ProjectConfig {
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class ProjectConfig {
+
+    public WebClient webClient() {
+        return WebClient.builder().build();
+    }
 }
