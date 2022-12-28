95a1980f504cd2e22e0601669122f207724fe431
Leo Sun <leosun@Leos-MacBook-Pro.local>
2022-12-28 20:39:11 +0800

    custom data source

---
+++ chapter12-datasource-custom/src/main/java/com/example/controller/PurchaseController.java
@@ -0,0 +1,28 @@
+package com.example.controller;
+
+import com.example.model.Purchase;
+import com.example.repository.PurchaseRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+
+    private final PurchaseRepository purchaseRepository;
+
+    public PurchaseController(PurchaseRepository purchaseRepository) {
+        this.purchaseRepository = purchaseRepository;
+    }
+
+    @PostMapping
+    public void storePurchase(@RequestBody Purchase purchase) {
+        purchaseRepository.storePurchase(purchase);
+    }
+
+    @GetMapping
+    public List<Purchase> findPurchases() {
+        return purchaseRepository.findAllPurchase();
+    }
+}
