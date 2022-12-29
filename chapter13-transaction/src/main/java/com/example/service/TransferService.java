e1d13bf4d326daf68fe36f69fd2fdf343ff270a8
Leo Sun <leosun@Leos-MacBook-Pro.local>
2022-12-29 19:49:35 +0800

    chapter13

--- chapter13-transaction/src/main/java/com/example/service/TransferService.java
+++ chapter13-transaction/src/main/java/com/example/service/TransferService.java
@@ -1,8 +1,38 @@
 package com.example.service;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.model.Account;
+import com.example.repository.AccountRepository;
 
 @Service
 public class TransferService {
-    
+
+    private final AccountRepository accountRepository;
+
+    public TransferService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Transactional
+    public void transferMoney(long idSender,
+            long idReceiver,
+            BigDecimal amount) {
+        Account sender = accountRepository.findAccountById(idSender);
+        Account receiver = accountRepository.findAccountById(idReceiver);
+
+        BigDecimal senderNewAmount = sender.getAmount().subtract(amount);
+        BigDecimal receiverNewAmount = receiver.getAmount().add(amount);
+
+        accountRepository.changeAmount(idSender, senderNewAmount);
+        accountRepository.changeAmount(idReceiver, receiverNewAmount);
+    }
+
+    public List<Account> getAllAccounts() {
+        return accountRepository.findAllAccounts();
+    }
 }
