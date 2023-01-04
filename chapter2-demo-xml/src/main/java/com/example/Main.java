86c76f21d4dc9b48fca10b21075af5b9b86902b5
Leo Sun <leosun@Leos-MacBook-Pro.local>
2023-01-04 18:17:40 +0800

    xml configuration

--- chapter2-demo-xml/src/main/java/com/example/Main.java
+++ chapter2-demo-xml/src/main/java/com/example/Main.java
@@ -1,22 +1,14 @@
 package com.example;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import com.example.config.ProjectConfig;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
-        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        var context = new ClassPathXmlApplicationContext("config.xml");
         
         Parrot parrot = context.getBean(Parrot.class);
         System.out.println(parrot.getName());
-        
-        String s = context.getBean(String.class);
-        System.out.println(s);
-        
-        Integer n = context.getBean(Integer.class);
-        System.out.println(n);
 
         context.close();
     }
