b7f6d6732bf5e84d4affcf8a3bf77773f27818c2
sundalei <sundalei1988@gmail.com>
2025-09-25 22:43:32 +0800

    Update version for spring framework and junit

--- chapter2-demo/src/main/java/com/example/Main.java
+++ chapter2-demo/src/main/java/com/example/Main.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import com.example.config.ProjectConfig;
 
 public class Main {
+
     public static void main(String[] args) {
 
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
