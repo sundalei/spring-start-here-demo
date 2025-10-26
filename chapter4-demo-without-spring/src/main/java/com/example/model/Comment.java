d7ead6395fa345664532b689198a244c8349dd7c
sundalei <sundalei1988@gmail.com>
2025-10-26 22:13:46 +0800

    Add chapter4-demo-without-spring module with basic Comment service, repository, and notification proxy implementation

---
+++ chapter4-demo-without-spring/src/main/java/com/example/model/Comment.java
@@ -0,0 +1,5 @@
+package com.example.model;
+
+public record Comment (String author, String text) {
+
+}
---
+++ chapter4-demo-without-spring/src/main/java/com/example/proxies/CommentNotificationProxy.java
@@ -0,0 +1,8 @@
+package com.example.proxies;
+
+import com.example.model.Comment;
+
+public interface CommentNotificationProxy {
+
+    void sendComment(Comment comment);
+}
