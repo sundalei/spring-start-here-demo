ea7af463a073d571734612e057cfc6621873ee1c
sundalei <sundalei1988@gmail.com>
2025-11-08 20:23:17 +0800

    style: remove unnecessary @ComponentScan annotation from ProjectConfig

--- chapter5-prototype-bean/src/main/java/com/example/config/ProjectConfig.java
+++ chapter5-prototype-bean/src/main/java/com/example/config/ProjectConfig.java
@@ -2,14 +2,12 @@ package com.example.config;
 
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 import com.example.services.CommentService;
 
 @Configuration
-@ComponentScan(basePackages = "com.example.services")
 public class ProjectConfig {
     
     @Bean
