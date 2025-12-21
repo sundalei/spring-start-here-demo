8cf239676e643d79edb35b9a0072592c62bace6a
sundalei <sundalei1988@gmail.com>
2025-12-21 15:53:47 +0800

    feat: add new `/home` endpoint with optional request parameters for name and color.

--- chapter8-template-demo/src/main/java/com/example/MainController.java
+++ chapter8-template-demo/src/main/java/com/example/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -11,6 +12,16 @@ public class MainController {
   @RequestMapping("/home/{color}")
   public String home(@PathVariable String color, Model page) {
     page.addAttribute("username", "Katy");
+    page.addAttribute("color", color);
+    return "home.html";
+  }
+
+  @RequestMapping("/home")
+  public String profile(
+      @RequestParam(required = false, defaultValue = "Katy") String name,
+      @RequestParam(required = false, defaultValue = "blue") String color,
+      Model page) {
+    page.addAttribute("username", name);
     page.addAttribute("color", color);
     return "home.html";
   }
