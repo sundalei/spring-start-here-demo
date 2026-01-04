4b12efc9511f5ea201e77d6a32982977d822e0ee
sundalei <sundalei1988@gmail.com>
2026-01-04 16:14:17 +0800

    refactor

--- spring-boot-demos/chapter9-loggin-demo/src/main/java/com/example/Application.java
+++ spring-boot-demos/chapter9-loggin-demo/src/main/java/com/example/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Chapter9LogginDemoApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Chapter9LogginDemoApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 }
