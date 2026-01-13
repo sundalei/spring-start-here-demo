6ceba37ac5e8bb8aeb751967340c6e4da692a02a
sundalei <sundalei1988@gmail.com>
2026-01-13 22:14:29 +0800

    fix(controller): simplify exception handler for NotEnoughMoneyException

--- spring-boot-demos/chapter10-rest-service/src/main/java/com/example/controller/ExceptionControllerAdvice.java
+++ spring-boot-demos/chapter10-rest-service/src/main/java/com/example/controller/ExceptionControllerAdvice.java
@@ -12,8 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ExceptionControllerAdvice {
 
   @ExceptionHandler(NotEnoughMoneyException.class)
-  public ResponseEntity<ErrorDetails> exceptionNotEnoughMoneyHandler(NotEnoughMoneyException e) {
-    log.info("exception handler for exception {}", e.getClass().getSimpleName());
+  public ResponseEntity<ErrorDetails> exceptionNotEnoughMoneyHandler() {
     ErrorDetails errorDetails = new ErrorDetails();
     errorDetails.setMessage("Not enough money to make the payment");
     return ResponseEntity.badRequest().body(errorDetails);
