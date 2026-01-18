c3194913df5024f73fd398dde68b6c10b9dbe62b
sundalei <sundalei1988@gmail.com>
2026-01-18 18:45:47 +0800

    refactor

--- spring-boot-demos/chapter11-resttemplate/src/main/java/com/example/Application.java
+++ spring-boot-demos/chapter11-resttemplate/src/main/java/com/example/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Chapter11ResttemplateApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Chapter11ResttemplateApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 }
