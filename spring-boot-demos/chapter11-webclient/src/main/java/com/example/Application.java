69e1b207d7021aa36c3f16deddeaf60d662984cd
sundalei <sundalei1988@gmail.com>
2026-01-22 22:41:46 +0800

    Refactor: Integrate chapter11-webclient into spring-boot-demos
    - Move chapter11-webclient into spring-boot-demos modules
    
    - Rename entry point to Application.java for consistency
    
    - Update pom.xml to inherit from spring-boot-demos parent
    
    - Clean up Maven wrapper files
    
    - Manage Netty version in parent pom

--- spring-boot-demos/chapter11-webclient/src/main/java/com/example/Application.java
+++ spring-boot-demos/chapter11-webclient/src/main/java/com/example/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Chapter11WebclientApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Chapter11WebclientApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 }
