66dc36f73d9285b398727a1a105b43351622fe57
sundalei <sundalei1988@gmail.com>
2026-01-29 13:20:49 +0900

    build: update spring-boot version to 4.0.2
    test: add basic test class for chapter12-purchase-jdbctemplate
    feat: add main application class for chapter12-purchase-jdbctemplate

--- spring-boot-demos/chapter12-purchase-jdbctemplate/src/main/java/com/example/Application.java
+++ spring-boot-demos/chapter12-purchase-jdbctemplate/src/main/java/com/example/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Chapter12PurchaseJdbctemplateApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Chapter12PurchaseJdbctemplateApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 }
