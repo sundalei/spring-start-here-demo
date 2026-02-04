2f037e3e285f38386b957c142c197ed780707b3c
sundalei <sundalei1988@gmail.com>
2026-02-04 21:18:37 +0900

    feat: add MySQL purchase module with REST API support
    - Add new module chapter12-purchase-mysql to parent pom
    - Implement purchase REST controller with repository
    - Configure MySQL datasource in application.yml
    - Create purchase table schema and model
    - Include basic test and application setup

--- spring-boot-demos/chapter12-purchase-mysql/src/test/java/com/example/ApplicationTests.java
+++ spring-boot-demos/chapter12-purchase-mysql/src/test/java/com/example/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Chapter12PurchaseMysqlApplicationTests {
+class ApplicationTests {
 
   @Test
   void contextLoads() {}
